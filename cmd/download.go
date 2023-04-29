59378c2ce752c769fcbffe9e7587a025fc51431c
Raeein <raeeinbagheri80@gmail.com>
2023-04-29 12:59:06 -0400

    add downloading

--- cmd/download.go
+++ cmd/download.go
@@ -2,8 +2,16 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"net/url"
+	"os"
+	"path"
+	"sync"
+	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/spf13/cobra"
 )
 
@@ -14,8 +22,8 @@ var downloadCmd = &cobra.Command{
     Run: func(cmd *cobra.Command, args []string) {
 
         allFlag, _ := cmd.Flags().GetBool("all")
-
         urlFlag, _ := cmd.Flags().GetString("url")
+        timeoutFlag, _ := cmd.Flags().GetInt("timeout")
 
         if urlFlag == "" {
             fmt.Println("URL is required")
@@ -30,7 +38,7 @@ var downloadCmd = &cobra.Command{
             return
         }
 
-        downloadFile(u, allFlag)
+        find_audios(u, allFlag, timeoutFlag)
     },
 }
 
@@ -38,6 +46,7 @@ func init() {
     rootCmd.AddCommand(downloadCmd)
     downloadCmd.Flags().BoolP("all", "a", false, "Download all files in a book")
     downloadCmd.Flags().StringP("url", "u", "", "URL to download")
+    downloadCmd.Flags().IntP("timeout", "t", 0, "HTTP timeout in seconds")
 }
 
 // each piece is a part of a music file to be download and concatenated laters on
@@ -55,9 +64,70 @@ func (b *book) addPiece(newPiece piece) {
     *b = append(*b, newPiece)
 }
 
-
-func downloadFile(url *url.URL, all bool) {
+func find_audios(url *url.URL, all bool, timeout int) {
     fmt.Println("URL:", url)
     fmt.Println("Downloading file...")
+
+    client := http.Client{}
+    if timeout != 0 && timeout > 0 {
+        client.Timeout = time.Duration(timeout) * time.Second
+    }
+
+    testUrl := "https://goldenaudiobook.com/rich-dad-poor-dad/"
+    resp, err := client.Get(testUrl)
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+    defer resp.Body.Close()
+    fmt.Println("Status code:", resp.StatusCode)
+
+    doc, err := goquery.NewDocumentFromReader(resp.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("Finding media elements...")
+
+    var audioUrls []string
+
+    findMediaElements(&audioUrls, doc)
+
+    var wg sync.WaitGroup
+    wg.Add(len(audioUrls))
+
+    for _, url := range audioUrls {
+        go downloadAudio(url, &wg)
+    }
+    wg.Wait()
+}
+
+func findMediaElements(urls *[]string, doc *goquery.Document) {
+    doc.Find(".wp-audio-shortcode").Each(func(i int, s *goquery.Selection) {
+        *urls = append(*urls, s.Text())
+    })
+}
+
+func downloadAudio(url string, wg *sync.WaitGroup) {
+    defer wg.Done()
+
+    fmt.Println("Downloading ", url)
+    fileName := path.Base(url)
+    output, err := os.Create(fileName)
+    if err != nil {
+        fmt.Println("Error while creating", fileName, "-", err)
+        return
+    }
+    defer output.Close()
+
+    response, err := http.Get(url)
+    if err != nil {
+        fmt.Println("Error while downloading", url, "-", err)
+        return
+    }
+    defer response.Body.Close()
+
+    io.Copy(output, response.Body)
+    fmt.Println("Downloaded", fileName)
 }
 
--- go.mod
+++ go.mod
@@ -2,9 +2,23 @@ module github.com/Raeein/DownloadIT
 
 go 1.20
 
-require github.com/spf13/cobra v1.7.0
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/spf13/cobra v1.7.0
+	golang.org/x/net v0.9.0
+)
 
 require (
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/term v0.7.0 // indirect
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/schollz/progressbar/v3 v3.13.1
 	github.com/spf13/pflag v1.0.5 // indirect
 )
